39311933f42b73ac2a84cfd21f402b574f73702e
Alison McCue <gh@maladroit.me>
2024-10-29 01:03:21 -0400

    Update .config/nix/flake.lock Update .config/nix/flake.nix

--- dot_config/nix/flake.nix
+++ dot_config/nix/flake.nix
@@ -5,20 +5,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-
-    # Optional: Declarative tap management
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
     };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-  };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
 
@@ -51,26 +40,6 @@
       fonts.packages = [
         (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
       ];
-
-#      system.activationScripts.applications.text = let
-#        env = pkgs.buildEnv {
-#          name = "system-applications";
-#          paths = config.environment.systemPackages;
-#          pathsToLink = "/Applications";
-#        };
-#      in
-#        pkgs.lib.mkForce ''
-#        # Set up applications.
-#        echo "setting up /Applications..." >&2
-#        rm -rf /Applications/Nix\ Apps
-#        mkdir -p /Applications/Nix\ Apps
-#        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-#        while read src; do
-#          app_name=$(basename "$src")
-#          echo "copying $src" >&2
-#          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-#        done
-#            '';
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
